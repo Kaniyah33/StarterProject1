a07899cdcd00af8901a047776a87e73882007204
Kaniyah33 <81383676+Kaniyah33@users.noreply.github.com>
2022-09-21 23:50:55 -0400

    Black box tests for boggle_solver.js

---
+++ boggle_solver.test.js
@@ -0,0 +1,160 @@
+const boggle_solver = require('/home/codio/workspace/Boggle_Testing/boggle_solver.js');
+
+/** Lowercases a string array in-place. (Used for case-insensitive string array
+ *  matching).
+ * @param {string[]} stringArray - String array to be lowercase.
+ */
+function lowercaseStringArray(stringArray) {
+  for (let i = 0; i < stringArray.length; i++)
+    stringArray[i] = stringArray[i].toLowerCase();
+}
+
+describe('Boggle Solver tests suite:', () => {
+  describe('Normal input', () => {
+    
+    test('Normal case 2x2', () =>{
+        //Tests a normal 2x2 grid.
+        let grid = [['A', 'B'],
+                    ['C', 'D']];
+        let dictionary = ['abc', 'abd', 'ada', 'bcd'];
+        let expected = ['abc', 'abd', 'bcd'];
+
+        let solutions = boggle_solver.findAllSolutions(grid, dictionary);
+
+      // Lowercasing for case-insensitive string array matching.
+      lowercaseStringArray(solutions);
+      lowercaseStringArray(expected);
+      expect(solutions.sort()).toEqual(expected.sort());
+    });
+    
+    test('Normal case 3x3', () => {
+      // Tests a normal 3x3 grid.
+      let grid = [['A', 'B', 'C'],
+                    ['D', 'E', 'F'],
+                    ['G', 'H', 'I']];
+      let dictionary = ['abc', 'abdhi', 'abi'];
+      let expected = ['abc', 'abdhi'];
+
+      let solutions = boggle_solver.findAllSolutions(grid, dictionary);
+
+      // Lowercasing for case-insensitive string array matching.
+      lowercaseStringArray(solutions);
+      lowercaseStringArray(expected);
+      expect(solutions.sort()).toEqual(expected.sort());
+    });
+
+    test('Normal case 4x4', () =>{
+        //Tests a normal 4x4 grid.
+        let grid = [['A', 'B', 'J', 'O'],
+                    ['C', 'D', 'E', 'K'],
+                    ['I', 'M', 'R', 'P'],
+                    ['L', 'U', 'N', 'Z']];
+        let dictionary = ['bad', 'acdz', 'dab', 'cab', 'joke', 'joker', 'dim', 'dime', 'rum', 'run', 'lime', 'lure', 'bcr'];
+        let expected = ['bad', 'dab', 'cab', 'joke', 'joker', 'dim', 'dime', 'rum', 'run', 'lime', 'lure'];
+
+        let solutions = boggle_solver.findAllSolutions(grid, dictionary);
+
+      // Lowercasing for case-insensitive string array matching.
+      lowercaseStringArray(solutions);
+      lowercaseStringArray(expected);
+      expect(solutions.sort()).toEqual(expected.sort());
+    });
+
+    test('Normal case 5x5', () =>{
+        //Tests a normal 5x5 grid.
+        let grid = [['A', 'B', 'J', 'O', 'C'],
+                    ['C', 'D', 'E', 'K', 'St'],
+                    ['I', 'M', 'R', 'P', 'A'],
+                    ['L', 'U', 'N', 'Z', 'Qu'],
+                    ['W', 'F', 'H', 'X', 'I']];
+        let dictionary = ['quiz', 'stake', 'whale', 'coke', 'quake', 'jerk', 'stork', 'zap'];
+        let expected = ['quiz', 'stake', 'coke', 'quake', 'jerk', 'zap'];
+
+        let solutions = boggle_solver.findAllSolutions(grid, dictionary);
+
+      // Lowercasing for case-insensitive string array matching.
+      lowercaseStringArray(solutions);
+      lowercaseStringArray(expected);
+      expect(solutions.sort()).toEqual(expected.sort());
+    });
+
+  });
+
+  
+  describe('Problem contraints', () => {
+    // Cases such as Qu
+    test('Works with Qu', () =>{
+      let grid = [['A', 'Qu'],
+                  ['B', 'C']];
+      let dictionary = ['AQu', 'BQu'];
+      let solutions = boggle_solver.findAllSolutions(grid, dictionary);
+      lowercaseStringArray(solutions);
+      lowercaseStringArray(dictionary);
+      expect(solutions.sort()).toEqual(dictionary.sort());
+    })
+ 
+    // Cases such as St
+    test('Works with St', () =>{
+      let grid = [['A', 'B'],
+                  ['St', 'C']];
+      let dictionary = ['ASt', 'BSt', 'StC'];
+      let solutions = boggle_solver.findAllSolutions(grid, dictionary);
+      lowercaseStringArray(solutions);
+      lowercaseStringArray(dictionary);
+      expect(solutions.sort()).toEqual(dictionary.sort());
+    })
+  });
+
+  
+  describe('Input edge cases', () => {
+
+    // Example Test using Jess
+    test('Dictionary is empty', () => {
+      // (Edge case) Since there are no possible solutiona, it should return an
+      // empty list.
+      let grid = [['A', 'B', 'C', 'D'],
+                    ['E', 'F', 'G', 'H'],
+                    ['I', 'J', 'K', 'L'],
+                    ['M', 'N', 'O', 'P']];
+      let dictionary = [];
+      let expected = [];
+
+      let solutions = boggle_solver.findAllSolutions(grid, dictionary);
+
+      // Lowercasing for case-insensitive string array matching.
+      lowercaseStringArray(solutions);
+      lowercaseStringArray(expected);
+      expect(solutions.sort()).toEqual(expected.sort());
+    });
+
+    test('Edge case 0x0', () => {
+      // Algorithm should return an empty list since the grid is empty
+      let grid = [[]];
+      let dictionary = ['good', 'morning', 'sunshine'];
+      let expected = [];
+
+      let solutions = boggle_solver.findAllSolutions(grid, dictionary);
+
+      // Lowercasing for case-insensitive string array matching.
+      lowercaseStringArray(solutions);
+      lowercaseStringArray(expected);
+      expect(solutions.sort()).toEqual(expected.sort());
+    });
+
+    test('Edge case 1x1', () => {
+      // Algorithm should return an empty list since there are less than 3 letters
+      let grid = [['A'],
+                  ['B']];
+      let dictionary = ['good', 'morning', 'sunshine', 'ab', 'ba'];
+      let expected = [];
+
+      let solutions = boggle_solver.findAllSolutions(grid, dictionary);
+
+      // Lowercasing for case-insensitive string array matching.
+      lowercaseStringArray(solutions);
+      lowercaseStringArray(expected);
+      expect(solutions.sort()).toEqual(expected.sort());
+    });
+
+  });
+});
